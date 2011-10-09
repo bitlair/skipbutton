bc4a0518b8058d8e6fecd9ca118d593a65bbaad3
Bob <bob-nospam-@xbmc.org>
2011-10-09 20:42:54 +0200

    removed: debug code

--- skipbutton_program/src/msgparser.cpp
+++ skipbutton_program/src/msgparser.cpp
@@ -24,14 +24,12 @@ void CMsgParser::Process(uint8_t byte)
   uint8_t prev = ~m_prev;
   if (byte == prev)
   {
-    //printf("accepted %02x\n", m_prev);
     m_msg.push_back(m_prev);
     ProcessMsg();
     m_prev = -1;
   }
   else
   {
-    //printf("rejected %02x\n", byte);
     m_msg.clear();
     m_prev = byte;
   }
@@ -39,14 +37,8 @@ void CMsgParser::Process(uint8_t byte)
 
 void CMsgParser::ProcessMsg()
 {
-  /*printf("msg:");
-  for (int i = 0; i < m_msg.size(); i++)
-    printf(" %02x", m_msg[i]);
-  printf("\n");*/
-
   if (m_msg[0] != (uint8_t)MSG_START || (m_msg.size() == 3 && m_msg[2] != (uint8_t)MSG_END))
   {
-    //printf("cleared %i\n", __LINE__);
     m_msg.clear();
     return;
   }
@@ -57,7 +49,6 @@ void CMsgParser::ProcessMsg()
     {
       printf("skip\n");
       system("curl -H \"Content-type:application/json\" -d '{\"action\":\"next\"}' http://192.168.88.10:5000/control");
-      //system("paplay beep.wav");
     }
     else if (m_msg[1] == MSG_VOLUP)
     {
